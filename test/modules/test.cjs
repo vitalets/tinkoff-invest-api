30774cfa2e854cac586d4572c4b815c2ffa5229c
Vitaliy Potapov <noginsk@rambler.ru>
2022-06-13 17:21:09 +0300

    exports without wildcard

--- test/modules/test.cjs
+++ test/modules/test.cjs
@@ -1,6 +1,6 @@
 const assert = require('assert');
 const { TinkoffInvestApi } = require('tinkoff-invest-api');
-const { CandleInterval } = require('tinkoff-invest-api/generated/marketdata');
+const { CandleInterval } = require('tinkoff-invest-api/marketdata');
 
 assert(new TinkoffInvestApi({ token: 'token' }));
 assert.equal(CandleInterval.CANDLE_INTERVAL_1_MIN, 1);
--- test/modules/test.mjs
+++ test/modules/test.mjs
@@ -1,6 +1,6 @@
 import assert from 'assert';
 import { TinkoffInvestApi } from 'tinkoff-invest-api';
-import { CandleInterval } from 'tinkoff-invest-api/generated/marketdata';
+import { CandleInterval } from 'tinkoff-invest-api/marketdata';
 
 assert(new TinkoffInvestApi({ token: 'token' }));
 assert.equal(CandleInterval.CANDLE_INTERVAL_1_MIN, 1);
