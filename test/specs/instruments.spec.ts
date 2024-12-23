116a79ec7c0f5f2e74fc69fdf2dbaebc9711ffb7
vitalets <noginsk@rambler.ru>
2024-12-23 12:24:14 +0400

    test: add test for api error

--- test/specs/instruments.spec.ts
+++ test/specs/instruments.spec.ts
@@ -1,4 +1,5 @@
 import { InstrumentIdType } from '../../src/generated/instruments.js';
+import { TinkoffApiError } from '../../src/index.js';
 
 describe('instruments', () => {
 
@@ -9,6 +10,21 @@ describe('instruments', () => {
       id: 'BBG0013HGFT4'
     });
     assert.equal(res.instrument?.name, 'Доллар США');
+  });
+
+  it('error: Instrument not found', async () => {
+    const promise = testApi.instruments.getInstrumentBy({
+      idType: InstrumentIdType.INSTRUMENT_ID_TYPE_FIGI,
+      classCode: '',
+      id: 'xxxxxxx'
+    });
+    await assert.rejects(promise, (error: TinkoffApiError) => {
+      assert.match(error.message, /Instrument not found/);
+      assert.match(error.ratelimit, /\d+/)
+      assert.match(error.ratelimitRemaining, /\d+/)
+      assert.match(error.ratelimitReset, /\d+/)
+      return true;
+    });
   });
 
 });
